de385c798e4195e1973196e7045b0635f8650590
LennyRamos <tuf18062@temple.edu>
2019-09-27 23:03:52 -0400

    checkpoint

--- src/app/delete-list/delete-list.component.ts
+++ src/app/delete-list/delete-list.component.ts
@@ -11,6 +11,7 @@ import { FoqusList } from '../_models/foqus-list';
 export class DeleteListComponent implements OnInit {
 
   @Input() list: FoqusList;
+  @Input() index: number;
 
   constructor(private foqusListService: FoqusListService, private foqusListsComponent: FoqusListsComponent) { }
 
@@ -21,12 +22,26 @@ export class DeleteListComponent implements OnInit {
    * For deleting a list item
    * @param id the id of the list to be deleted
    */
-  deleteList(id: number) {
-    this.foqusListService.removeListItem(id)
+  private deleteList() {
+    console.log(this.list.id);
+
+    this.foqusListService.removeListItem(this.list.id)
     .subscribe(res => console.log(res));
 
     console.log('Just delete list');
+    console.log(this.list.id);
+  }
+
+  /**
+   * A test to make sure button passes id
+   * @param id List to be deleted
+   */
+  private getListToBeDeletedID(id?: number) {
+    console.log(this.list.id);
+    console.log('Previous value ' + this.list.id);
     console.log(id);
+    console.log(this.list.id);
+
   }
 
 }
