5caf7ba968adaf3c8e686b3cad7986d1958699aa
LennyRamos <tuf18062@temple.edu>
2019-10-06 13:29:35 -0400

    login UI

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +11,22 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   message: string;
 
+  loginCredential = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
   constructor(public authService: AuthService, public router: Router) {
     this.setMessage();
   }
 
   setMessage() {
     this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
+  }
+
+  goToRegisterPage(): void {
+    console.log('about to go to register page');
+    this.router.navigate(['/register']);
   }
 
   login() {
@@ -26,7 +37,7 @@ export class LoginComponent {
       if (this.authService.isLoggedIn) {
         // Get the redirect URL from our auth service
         // If no redirect has been set, use the default
-        let redirect = this.authService.redirectUrl ? this.router.parseUrl(this.authService.redirectUrl) : '/';
+        const redirect = this.authService.redirectUrl ? this.router.parseUrl(this.authService.redirectUrl) : '/';
 
         // Redirect the user
         this.router.navigateByUrl(redirect);
